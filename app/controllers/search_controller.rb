7c2cbb9adbecf5817d9276f98c23076cd75637f8
Uuya <a1b23cd@icloud.com>
2020-02-16 19:54:17 +0900

    検索機能しょぼver

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,10 @@
 class SearchController < ApplicationController
+
+  def index
+    @categories = Category.all
+    @params = params[:q]
+    @q = Book.ransack(params[:q])
+    @books = @q.result(distinct: true)
+  end
+
 end
